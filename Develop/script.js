f6d804bb478d3452c9998e0c79b572753f29939d
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-01-08 22:58:28 -0600

    random character generation for password

--- Develop/script.js
+++ Develop/script.js
@@ -1,86 +1,205 @@
 // Assignment Code
 //var generateBtn = document.querySelector("#generate");
 
-var letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var letterArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var symbolArray = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "-", "`", ",", ".", "[", "{", "]", "}", "/", ":", ";", "<", ">", "?", "'"];
+var symbolArray = [
+  "~",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "+",
+  "=",
+  "-",
+  "`",
+  ",",
+  ".",
+  "[",
+  "{",
+  "]",
+  "}",
+  "/",
+  ":",
+  ";",
+  "<",
+  ">",
+  "?",
+  "'",
+];
 
-var lowerCase = function() {
-  let x = Math.floor(Math.random() * 26);
-  let letter = letterArray[x].toString();
-  return letter;
-};
-
-var upperCase = function() {
-  let x = Math.floor(Math.random() * 26);
-  let letter = letterArray[x].toString();
-  letter = letter.toUpperCase();
-  return letter;
-};
-
-var number = function() {
-  let x = Math.floor(Math.random() * 10);
-  let num = numberArray[x].toString();
-  return num;
-};
-
-var symbol = function() {
-  let x = Math.floor(Math.random() * 29);
-  let sym = symbolArray[x].toString();
-  return sym;
+var character = {
+  lowerCase: function () {
+    let x = Math.floor(Math.random() * 26);
+    let letter = letterArray[x].toString();
+    return letter;
+  },
+  upperCase: function () {
+    let x = Math.floor(Math.random() * 26);
+    let letter = letterArray[x].toString();
+    letter = letter.toUpperCase();
+    return letter;
+  },
+  number: function () {
+    let x = Math.floor(Math.random() * 10);
+    let num = numberArray[x].toString();
+    return num;
+  },
+  symbol: function () {
+    let x = Math.floor(Math.random() * 29);
+    let sym = symbolArray[x].toString();
+    return sym;
+  },
 };
 
 document.getElementById("generate").onclick = clicked;
 
-var getLowerCase = function() {
-  let answerLowerCase = window.confirm("Would you like your password to include lowercase letters?");
+function generatePassword() {
+  let answerRange = window.prompt(
+    "How long would you like your password to be? You may select at least 8 and at most 128 characters."
+  );
+  answerRange = parseInt(answerRange);
+
+  if (answerRange >= 8 && answerRange <= 128) {
+    sessionStorage.setItem("range", answerRange);
+  } else {
+    window.alert("Sorry, please input a number from 8 to 128");
+    generatePassword();
+  }
+
+  let answerLowerCase = window.confirm(
+    "Would you like your password to include lowercase letters?"
+  );
   sessionStorage.setItem("lowerCase", answerLowerCase);
-};
 
-var getUpperCase = function() {
-  let answerUpperCase = window.confirm("Would you like it to include uppercase letters?");
+  let answerUpperCase = window.confirm(
+    "Would you like it to include uppercase letters?"
+  );
   sessionStorage.setItem("upperCase", answerUpperCase);
-};
 
-var getNumbers = function() {
   let answerNumbers = window.confirm("Would you like it to include numbers?");
   sessionStorage.setItem("numbers", answerNumbers);
-};
 
-var getSymbols = function() {
   let answerSymbols = window.confirm("Would you like it to include symbols?");
   sessionStorage.setItem("symbols", answerSymbols);
-};
 
-function clicked() {
-  
-  if (clicked) {
+  let range = sessionStorage.getItem("range");
+  let lettersL = sessionStorage.getItem("lowerCase").toString();
+  let lettersU = sessionStorage.getItem("upperCase").toString();
+  let numbers = sessionStorage.getItem("numbers").toString();
+  let symbols = sessionStorage.getItem("symbols").toString();
 
-    let answerRange = window.prompt("How long would you like your password to be? You may select at least 8 and at most 128 characters.");
-    answerRange = parseInt(answerRange);
+  let charLow = [];
+  let charUpp = [];
+  let charNum = [];
+  let charSym = [];
 
-    if (answerRange >= 8 && answerRange <= 128) {
-      sessionStorage.setItem("range", answerRange);
-    } else {
-      window.alert("Sorry, please input a number from 8 to 128");
-      clicked();
+  let charSet = [];
+
+  if (lettersL === "true") {
+    let i = 0;
+    while (i < range) { 
+      charLow.push(character.lowerCase());
+      i++;
+    }
+    charSet.push(charLow);
+  }
+  if (lettersU === "true") {
+    let i = 0;
+    while (i < range) { 
+      charUpp.push(character.upperCase());
+      i++;
+    }
+    charSet.push(charUpp);
+  }
+  if (numbers === "true") {
+    let i = 0;
+    while (i < range) { 
+      charNum.push(character.number());
+      i++;
+    }
+    charSet.push(charNum);
+  }
+  if (symbols === "true") {
+    let i = 0;
+    while (i < range) { 
+      charSym.push(character.symbol());
+      i++;
+    } 
+    charSet.push(charSym);
+  }
+
+  var password = function() {
+
+    let randomNumber = function(maxNumber) {
+      let value = Math.floor(Math.random() * maxNumber);
+      return value;
     }
 
-    getLowerCase();
-    getUpperCase();
-    getNumbers();
-    getSymbols();
+    // shuffle function code sourced from Laurens Holst and ashleedawg through stackoverflow.com: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    let shuffleArray = function(array) {
+      for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
 
-    let range = sessionStorage.getItem("range");
-    let lettersL = sessionStorage.getItem("lowerCase");
-    let lettersU = sessionStorage.getItem("upperCase");
-    let numbers = sessionStorage.getItem("numbers");
-    let symbols = sessionStorage.getItem("symbols");
+    let characters = [];
 
-    window.alert(range);
+    for (var i = 0; i < range; i++) {
+      let value = charSet[randomNumber(charSet.length)];
+      characters.push(value[i].toString());
+    }
+
+    shuffleArray(characters);
+
+    console.log(characters);
   }
-};
 
+  password();
+}
+
+function clicked() {
+  if (clicked) {
+    generatePassword();
+  }
+}
 
 // Write password to the #password input
 //function writePassword() {
